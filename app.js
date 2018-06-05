78bd25214183849437dbc8d06c5ca3099742a9c0
Cook1619 <cook6348@gmail.com>
2018-06-05 12:05:27 -0500

    Event listerner is being added to each h2, but the alert needs to be out of the loop

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
-h3 = document.createElement('h3');
 
-for(var i = 0; i < 101; i++){
-    h3text = document.createTextNode('Accusation ' + [i] + ', ');
+var friends = ['Cody', 'Mike', 'Tom', 'Ahmed', 'Aaron'];
+var locations = ['kitchen', 'office', 'foyer', 'balcony', 'Master Bedroom', 'Printshop', 'Warehouse', 'Roof', 'Parking lot', 'Pipe Shop'];
+var weapons = ['pencil', 'knife', 'rubix cube', 'dymo printer', 'macbook', 'cell phone', 'dot-matrix printer', 'coffee cup', 'usb', 'mouse', 'keyboard', 'clock', 'credit departments dreams', 'coat hanger', 'tablet', 'compressed air', 'isopropyl alcohol', 'stapler', 'power strip', 'mini fridge'];
+
+for (var i = 0; i < 101; i++) {
+    var h3text = document.createTextNode('Accusation ' + [i] + ', ');
+    var h3 = document.createElement('h3');
     h3.appendChild(h3text);
     document.body.appendChild(h3);
+    h3 = document.querySelector('h3');
+    h3.addEventListener("click", function(){
+        alert('clicked');
+    })
+    
 }
 
-h3.addEventListener('click', function(){
-    for ( var i = 0; i < 101; i++){
-        alert('Accusation '+ [i] +' : I accuse Jane, with the paper clip in the copy room!')
-    }
-})
 
-var friends = ['Cody','Mike','Tom','Ahmed','Aaron'];
-var locations = ['kitchen','office','foyer','balcony','Master Bedroom','Printshop','Warehouse','Roof','Parking lot','Pipe Shop'];
-var weapons = ['pencil','knife','rubix cube','dymo printer','macbook','cell phone','dot-matrix printer','coffee cup','usb','mouse','keyboard','clock','credit departments dreams','coat hanger','tablet','compressed air','isopropyl alcohol','stapler','power strip','mini fridge'];
