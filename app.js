f0efc8cf162517d0eb1ba104fcc6d27e1b608a3b
Cook1619 <cook6348@gmail.com>
2018-06-05 12:43:04 -0500

    fixed some punctuation on last iteration of loop

--- app.js
+++ app.js
@@ -3,15 +3,19 @@ var friends = ['Cody', 'Mike', 'Tom', 'Ahmed', 'Aaron'];
 var locations = ['kitchen', 'office', 'foyer', 'balcony', 'Master Bedroom', 'Printshop', 'Warehouse', 'Roof', 'Parking lot', 'Pipe Shop'];
 var weapons = ['pencil', 'knife', 'rubix cube', 'dymo printer', 'macbook', 'cell phone', 'dot-matrix printer', 'coffee cup', 'usb', 'mouse', 'keyboard', 'clock', 'credit departments dreams', 'coat hanger', 'tablet', 'compressed air', 'isopropyl alcohol', 'stapler', 'power strip', 'mini fridge'];
 
-    for (var i = 0; i < 101; i++) {
+for (var i = 0; i < 101; i++) {
+    if (i === 100) {
+        var h3text = document.createTextNode('Accusation ' + [i]);
+    } else {
         var h3text = document.createTextNode('Accusation ' + [i] + ', ');
-        var h3 = document.createElement('h3');
-        h3.appendChild(h3text);
-        document.body.appendChild(h3);
-        h3 = document.querySelector('h3');
-        h3.addEventListener("click", alert);
     }
-   
-    function alert(){
-        alert('clicked!');
-    }
+    var h3 = document.createElement('h3');
+    h3.appendChild(h3text);
+    document.body.appendChild(h3);
+    h3 = document.querySelector('h3');
+    h3.addEventListener("click", alert);
+}
+
+function alert() {
+    alert('clicked!');
+}
