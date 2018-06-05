afbd1ba345eabbbd8683b289ed6a278d70fd8a2f
Cook1619 <cook6348@gmail.com>
2018-06-05 13:27:38 -0500

    Not quite the way the instructions want it, but it works. If you want a different alert, you need to refresh the page

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ var friends = ['Cody', 'Mike', 'Tom', 'Ahmed', 'Aaron'];
 var locations = ['kitchen', 'office', 'foyer', 'balcony', 'Master Bedroom', 'Printshop', 'Warehouse', 'Roof', 'Parking lot', 'Pipe Shop'];
 var weapons = ['pencil', 'knife', 'rubix cube', 'dymo printer', 'macbook', 'cell phone', 'dot-matrix printer', 'coffee cup', 'usb', 'mouse', 'keyboard', 'clock', 'credit departments dreams', 'coat hanger', 'tablet', 'compressed air', 'isopropyl alcohol', 'stapler', 'power strip', 'mini fridge'];
 
+var randFriends = friends[Math.floor(Math.random() * friends.length)];
+var randLocs = locations[Math.floor(Math.random() * locations.length)];
+var randWeaps = weapons[Math.floor(Math.random() * weapons.length)];
+
 for (var i = 0; i < 101; i++) {
     if (i === 100) {
         var h3text = document.createTextNode('Accusation ' + [i]);
@@ -16,5 +20,5 @@ for (var i = 0; i < 101; i++) {
 }
 
 function alertIt() {
-    alert(`I accuse ${friends[1]}, with the ${weapons[6]} in the ${locations[3]}!!`)
+    alert(`I accuse ${randFriends}, with the ${randWeaps} in the ${randLocs}!!`)
 }
